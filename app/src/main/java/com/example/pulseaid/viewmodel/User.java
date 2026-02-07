8e65348994a42c08c154f6bfe5e25052dc9b57d4
venushathi <148311558+venushathi@users.noreply.github.com>
2026-02-07 18:27:17 +0530

    Added customId field to User model

--- app/src/main/java/com/example/pulseaid/viewmodel/User.java
+++ app/src/main/java/com/example/pulseaid/viewmodel/User.java
@@ -2,6 +2,7 @@ package com.example.pulseaid.viewmodel;
 
 public class User {
     private String uid;
+    private String customId;
     private String name;
     private String email;
     private String role; // Roles: Admin , blood Staff , Hospital Staff , Donor
@@ -12,8 +13,9 @@ public class User {
     }
 
     // Main constructor to create a new user object
-    public User(String uid, String name, String email, String role, String bloodType) {
+    public User(String uid, String customId, String name, String email, String role, String bloodType) {
         this.uid = uid;
+        this.customId = customId;
         this.name = name;
         this.email = email;
         this.role = role;
@@ -22,6 +24,7 @@ public class User {
 
     // Getters
     public String getUid() { return uid; }
+    public String getCustomId() { return customId; }
     public String getName() { return name; }
     public String getEmail() { return email; }
     public String getRole() { return role; }
