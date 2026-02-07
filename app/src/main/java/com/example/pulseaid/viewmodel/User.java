46330910a7a4ad68c995d0ecada82c0abc3b952c
venushathi <148311558+venushathi@users.noreply.github.com>
2026-02-07 15:52:06 +0530

    Set up MVVM package structure

---
+++ app/src/main/java/com/example/pulseaid/viewmodel/User.java
@@ -0,0 +1,29 @@
+package com.example.pulseaid.viewmodel;
+
+public class User {
+    private String uid;
+    private String name;
+    private String email;
+    private String role; // Roles: Admin , blood Staff , Hospital Staff , Donor
+    private String bloodType; // Only required if the user is a donor
+
+    // Default constructor
+    public User() {
+    }
+
+    // Main constructor to create a new user object
+    public User(String uid, String name, String email, String role, String bloodType) {
+        this.uid = uid;
+        this.name = name;
+        this.email = email;
+        this.role = role;
+        this.bloodType = bloodType;
+    }
+
+    // Getters
+    public String getUid() { return uid; }
+    public String getName() { return name; }
+    public String getEmail() { return email; }
+    public String getRole() { return role; }
+    public String getBloodType() { return bloodType; }
+}
