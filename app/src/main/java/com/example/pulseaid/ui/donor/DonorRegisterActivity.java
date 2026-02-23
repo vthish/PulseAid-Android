57132d90446fdb7647af4409e225828bb1726d17
Chamira Hashan <chamirahashan7@gmail.com>
2026-02-23 15:19:46 +0530

    setup donor package

---
+++ app/src/main/java/com/example/pulseaid/ui/donor/DonorRegisterActivity.java
@@ -0,0 +1,13 @@
+package com.example.pulseaid.ui.donor;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.pulseaid.R;
+
+public class DonorRegisterActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donor_register);
+    }
+}
