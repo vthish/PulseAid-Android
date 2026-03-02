3bf7d835ebcfe4c0381dbcadec9dd9ed80901f40
venushathi <148311558+venushathi@users.noreply.github.com>
2026-03-02 21:51:22 +0530

    Change some files to match donor

--- app/src/main/java/com/example/pulseaid/data/admin/Donor.java
+++ app/src/main/java/com/example/pulseaid/data/admin/Donor.java
@@ -1,28 +1,32 @@
-package com.example.pulseaid.data;
+package com.example.pulseaid.data.admin;
 
-public class User {
+public class Donor {
     private String uid;
     private String name;
     private String email;
     private String role;
+    private String bloodGroup;
 
-    public User() {
+    public Donor() {
     }
 
-    public User(String uid, String name, String email, String role) {
+    public Donor(String uid, String name, String email, String role, String bloodGroup) {
         this.uid = uid;
         this.name = name;
         this.email = email;
         this.role = role;
+        this.bloodGroup = bloodGroup;
     }
 
     public String getUid() { return uid; }
     public String getName() { return name; }
     public String getEmail() { return email; }
     public String getRole() { return role; }
+    public String getBloodGroup() { return bloodGroup; }
 
     public void setUid(String uid) { this.uid = uid; }
     public void setName(String name) { this.name = name; }
     public void setEmail(String email) { this.email = email; }
     public void setRole(String role) { this.role = role; }
+    public void setBloodGroup(String bloodGroup) { this.bloodGroup = bloodGroup; }
 }
