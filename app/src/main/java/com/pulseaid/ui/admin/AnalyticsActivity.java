ac6541863091944eba986b0e8153cdcc4dd3a334
venushathi <148311558+vthish@users.noreply.github.com>
2026-04-26 02:15:34 +0530

    update analytics activity

--- app/src/main/java/com/pulseaid/ui/admin/AnalyticsActivity.java
+++ app/src/main/java/com/pulseaid/ui/admin/AnalyticsActivity.java
@@ -10,9 +10,6 @@ import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.pulseaid.R;
 import com.github.mikephil.charting.charts.PieChart;
@@ -42,15 +39,6 @@ public class AnalyticsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_analytics);
-
-        View mainView = findViewById(R.id.main);
-        if (mainView != null) {
-            ViewCompat.setOnApplyWindowInsetsListener(mainView, (v, insets) -> {
-                Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-                return insets;
-            });
-        }
 
         db = FirebaseFirestore.getInstance();
 
@@ -138,6 +126,6 @@ public class AnalyticsActivity extends AppCompatActivity {
         data.setValueTextColor(Color.WHITE);
 
         pieChartBloodGroups.setData(data);
-        pieChartBloodGroups.invalidate(); // Refresh the chart
+        pieChartBloodGroups.invalidate();
     }
 }
